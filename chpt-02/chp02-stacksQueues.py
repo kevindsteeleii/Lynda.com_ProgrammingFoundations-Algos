bb595384c979d06e80106467f3d20b281f8a4116
github username <github email address>
2019-06-28 09:33:30 -0400

    started adding some dunder/magic methods

--- chpt-02/chp02-stacksQueues.py
+++ chpt-02/chp02-stacksQueues.py
@@ -1,9 +1,16 @@
 class Stack:
     def __init__(self):
         self.stack = list()
+        self._count = 1
 
     def push(self, val):
         self.stack.append(val)
+        self._count += 1
 
     def pop(self):
         self.stack.pop()
+        self._count -= 1
+
+    @property
+    def count(self):
+        return self._count
